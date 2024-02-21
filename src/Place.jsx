5689238cf641452d02b229340fe63d017fc4786c
Vyshnavi <97187233+Hamsikakrishnan@users.noreply.github.com>
2024-02-21 19:54:21 +0530

    Updated

--- src/Place.jsx
+++ src/Place.jsx
@@ -2,7 +2,7 @@ export default function Place(props){
     return(
         <>
             <div className="place">
-                <img src={props.img} width="200"></img>
+                <img src={`../public/images/${props.img}`} width="200"></img>
                 <div className="info">
                     <h4 className="title">{props.title}</h4>
                     <p className="desc">{props.desc}</p>
