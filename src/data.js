5d1681c2bd402e37d588e1492008a7cad32d870b
Vyshnavi <97187233+Hamsikakrishnan@users.noreply.github.com>
2024-02-21 20:19:11 +0530

    Updated

--- src/data.js
+++ src/data.js
@@ -1,37 +1,37 @@
 export default[
     {
         id: 1,
-        img: "charminar.jpg",
+        img: "/./images/charminar.jpg",
         title: "Charminar",
         desc:"This square shaped structure with a height of 56 m and breadth of 30 m was constructed in 1591. It was built by Sultan Mohammed Quli Qutb Shah in the honor of his wife Bhagmati and to celebrate the end of a deadly plague."
     },
     {
         id: 2,
-        img: "golconda.jpg",
+        img: "/./images/golconda.jpg",
         title: "Golconda Fort",
         desc:"Its impressive structure has majestic walls which are as high as 15 to 18 foot and a parameter of close to 11 km. Along with brilliant architecture, this fort also captivates its visitors with its system of acoustics and water system."
     },
     {
         id: 3,
-        img:"ramoji.jpg",
+        img:"/./images/ramoji.jpg",
         title: "Ramoji Film City",
         desc: "Ramoji Film City has been alluring tourists from every corner of the country. One of the largest film studios in the world it has the capacity to house almost 50 film units. It is also listed in Guinness Book of World Records as well."
     },
     {
         id: 4,
-        img: "birla.jpg",
+        img: "/./images/birla.jpg",
         title: "Birla Mandir",
         desc: "Located atop 280-foot-high of Kalapahad, the beautiful Birla Mandir has derived its name from the industrialist Birlas .It took close to 2000 tonnes of white marbles to build this stunning temple bought from Rajasthan."
     },
     {
         id: 5,
         title: "Chowmahalla Palace",
-        img: "c1.jpg",
+        img: "/./images/c1.jpg",
         desc: "Chowmahalla Palace, with literal meaning of ‘four palaces’ in Urdu language was built in the 18th century. It was during the fifth Nizam, Afzar-ud-Daulah, Asaf Jav V, from 1857 to 1869, that the palace was completed."
     },
     {
         id: 6,
-        img: "tankbund.jpg",
+        img: "/./images/tankbund.jpg",
         title: "Tank Bund",
         desc: "Connecting Hyderabad and Secunderabad, Tank Bund is a popular hangout of the city. Promising its visitors with a breath taking view of the lake and its monolith of Bhudda in the center, it dams the Hussain Sagar Lake."
     }
